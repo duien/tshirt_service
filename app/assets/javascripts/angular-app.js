e17d8511bec090b8bb7864dbddfe1aa5fbb0cbf8
Emily Hyland <ehyland@newrelic.com>
2014-06-23 12:58:19 -0700

    Add a baby directive

--- app/assets/javascripts/angular-app.js
+++ app/assets/javascripts/angular-app.js
@@ -3,3 +3,16 @@ TshirtService = angular.module('tshirtService', []);
 TshirtService.controller('TestCtrl', ['$scope', function($scope){
   $scope.message = 'Hello';
 }]);
+
+TshirtService.directive('canHasTemplate', [function(){
+  return {
+      restrict: 'E',
+      templateUrl: 'templates/test.html',
+      scope: {
+        message: '='
+      },
+      // controller: ['$scope', function ($scope) {
+      //   $scope.
+      // }]
+    };
+}]);
